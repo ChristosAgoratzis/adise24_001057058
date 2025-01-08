1d8a9fb6d20d6acd20c9778a9a52c7a74f2c276f
Karagewrgos <dimosk48@gmail.com>
2025-01-08 22:53:43 +0200

    Add files via upload

--- js/success.js
+++ js/success.js
@@ -16,18 +16,25 @@ document.addEventListener('DOMContentLoaded', function() {
     }
 });
 
-
+let Timeout =false;
 function checkForOtherUser() {
     fetch("../lib/check_status.php")
         .then(response => response.json())
         .then(data => {
             if (!(data.status === "inactive")) {
+                clearTimeout(inactiveTimeout);
                 document.querySelector('.overlay').style.display = 'none';
                 document.querySelector('.popup').style.display = 'none';
+                window.location.href = '../html/ind.html';
             }else{
-                document.querySelector('.overlay').style.display = 'block';
-                document.querySelector('.popup').style.display = 'flex';
-                
+                if (Timeout == false){
+                    inactiveTimeout = setTimeout(() => {
+                        enterSinglePlayerMode();
+                    }, 5000);  
+                    Timeout = true;
+                    document.querySelector('.overlay').style.display = 'block';
+                    document.querySelector('.popup').style.display = 'flex';
+                }
             }
         })
         .catch(error => console.error("Error checking session status:", error));
@@ -36,3 +43,23 @@ function checkForOtherUser() {
 // Έλεγχος κάθε 5 δευτερόλεπτα
 setInterval(checkForOtherUser, 5000);
 
+function enterSinglePlayerMode() {
+    document.querySelector('.overlay').style.display = 'none';
+    document.querySelector('.popup').style.display = 'none';
+    alert("Switching to Single Player Mode...");
+    window.location.href = '../html/ind.html';
+//     fetch('../lib/updateisactive.php', {
+//         method: 'GET'
+//     }) .then(response =>{
+//         if(!response.ok){
+//             throw new Error('Network response was not ok' + response.statusText);
+//         }
+//         return response.json();
+//     })
+//     .then(data => {
+//         console.log(data);
+//     })
+//     .catch(error =>{
+//         console.error('There was a problem with the fetch operation:', error);
+//     });
+}
